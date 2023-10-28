4636f4f8d90f0eb04adf318f899b8214e13364d0
jkyvig <jkyvig@outlook.com>
2023-10-28 16:41:28 -0500

    updated js with no broken lines

--- src/app.js
+++ src/app.js
@@ -58,8 +58,8 @@ function displayForecast(response) {
 function getForecast(coordinates) {
     console.log(coordinates)
     let apiKey="cabdbda40038ba7d1165b953b1c7bd6c";
-    let apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}
-    &lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+    let apiURL = 
+    `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
     axios.get(apiURL).then(displayForecast);
 }
 
