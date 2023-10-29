a53174251de41f385f9b16aa22e1939b90233567
jkyvig <jkyvig@outlook.com>
2023-10-28 19:39:00 -0500

    removed fahrenheight

--- src/app.js
+++ src/app.js
@@ -96,35 +96,10 @@ function handleSubmit(event) {
     search(cityInputElement.value);
 }
 
-    function displayFahrenheightTemperature(event) {
-        event.preventDefault();
-        let temperatureElement = document.querySelector("#temperature");
-        celsiusLink.classList.remove("active");
-        fahrenheightLink.classList.add("active");
-        let fahrenheightTemperature = (celsiusTemperature * 9)/5 + 32;
-        temperatureElement.innerHTML = Math.round(fahrenheightTemperature);
-    }
-
-    function displayCelsiusTemperature(event) {
-        event.preventDefault();
-        celsiusLink.classList.add("active");
-        fahrenheightLink.classList.remove("active");
-        let temperatureElement = document.querySelector("#temperature");
-        temperatureElement.innerHTML = Math.round(celsiusTemperature);
-          
-    }
-
-
-
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
 
-let fahrenheightLink = document.querySelector("#fahrenheight-link");
-fahrenheightLink.addEventListener("click", displayFahrenheightTemperature);
-
-let celsiusLink = document.querySelector("#celsius-link");
-celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("New York");
 
