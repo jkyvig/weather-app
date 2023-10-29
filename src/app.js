a62feebb83bebd8b1a56855b173700a8fdf894b0
jkyvig <jkyvig@outlook.com>
2023-10-29 11:59:35 -0500

    fixed the inner HTML of date

--- src/app.js
+++ src/app.js
@@ -78,7 +78,7 @@ function getForecast(coordinates) {
     descriptionElement.innerHTML = response.data.weather[0].description;
     humidityElement.innerHTML = response.data.main.humidity;
     windElement.innerHTML = Math.round(response.data.wind.speed);
-    dateElement.innerHTML = formatDate(response.data.dt + 1000);
+    dateElement.innerHTML = formatDate(response.data.dt * 1000);
     iconElement.setAttribute("src",`https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
     iconElement.setAttribute("alt", response.data.weather[0].description);
     
